937ea98f2486c0de7c928dd5e17c9bbeacfff930
wari <nicolaycamacho@gmail.com>
2022-07-15 16:57:41 +0200

    content adapted and tailwind themes created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,9 @@ import {
   FaTimes,
   FaGithub,
   FaLinkedin,
-  FaFacebook,
-  FaLinkedinIn,
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-import Logo from '../assets/logo.png';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
@@ -17,9 +14,9 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-primary-default text-primary-text'>
       <div>
-        <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
+        {/* <img src={Logo} alt='Logo Image' style={{ width: '100px' }} /> */}
       </div>
 
       {/* menu */}
@@ -61,7 +58,7 @@ const Navbar = () => {
         className={
           !nav
             ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            : 'absolute top-0 left-0 w-full h-screen bg-primary-default flex flex-col justify-center items-center'
         }
       >
         <li className='py-6 text-4xl'>
@@ -100,32 +97,32 @@ const Navbar = () => {
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              className='flex justify-between items-center w-full text-primary-text'
+              href='https://www.linkedin.com/in/nicolay-camacho-maldonado-351bb391/' target='_blank' rel='noreferrer'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              className='flex justify-between items-center w-full text-primary-text'
+              href='https://github.com/wari1986' target='_blank' rel='noreferrer'
             >
               Github <FaGithub size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              className='flex justify-between items-center w-full text-primary-text'
+              href='mailto:nicolaycamacho@gmail.com' target='_blank' rel='noreferrer'
             >
               Email <HiOutlineMail size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              className='flex justify-between items-center w-full text-primary-text'
+              href='../assets/cv.pdf' download="CV-Nicolay-Camacho" target='_blank' rel='noreferrer'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
